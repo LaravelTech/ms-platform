f6a5b2bbf78840166eb883021005350c4bc9a820
trieuconcrete <trieu@concrete.com>
2020-02-18 13:29:37 +0700

    customize style admin

--- src/is-flatform/resources/views/layouts/admin.blade.php
+++ src/is-flatform/resources/views/layouts/admin.blade.php
@@ -13,9 +13,9 @@
   <link rel="stylesheet" href="{{ asset('assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset('assets/dist/css/adminlte.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('assets/dist/css/adminlte.css') }}">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('assets/dist/css/admin.css') }}">
 </head>
 <body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed">
 <div class="wrapper">
--- src/is-flatform/resources/views/layouts/partials/sidebar.blade.php
+++ src/is-flatform/resources/views/layouts/partials/sidebar.blade.php
@@ -2,7 +2,7 @@
   <!-- Brand Logo -->
   <a href="/" class="brand-link">
     <img src="{{ asset('assets/dist/img/logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-    <span class="brand-text font-weight-light">HR Internal</span>
+    <span class="brand-text font-weight-light">Internal System</span>
   </a>
   <!-- Sidebar -->
   <div class="sidebar">
