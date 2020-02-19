e631188855d7165ab0076affa37f29388b8e3508
trieuconcrete <trieu@concrete.com>
2020-02-19 20:46:56 +0700

    update code admin

--- src/is-flatform/resources/views/layouts/partials/header.blade.php
+++ src/is-flatform/resources/views/layouts/partials/header.blade.php
@@ -4,11 +4,6 @@
     <li class="nav-item">
       <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
     </li>
-    <li class="nav-item d-none d-sm-inline-block">
-      <a class="nav-link">
-        Welcome Administrator
-      </a>
-    </li>
   </ul>
 
   <!-- Right navbar links -->
